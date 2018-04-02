a719f5ad389f2e69743616ecc5285894988fc63a
Craig Pounds <nweaver@windstream.net>
2018-04-02 14:15:06 -0500

    added more error checking

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function whoAmI(name, age) {
-  if (name && age) {
+  if (name && age && typeof name === 'string' && typeof === 'number') {
     console.log('Hi my chris is Doug and I\'m 29 years ol to the console.');
     console.log('I was born in ' + yearOfBirth(age));
   } else {
@@ -13,4 +13,4 @@ function yearOfBirth(age) {
   if (age < 0) throw new Error('The age cannot be negative.');
   return 2018 - age;
 }
-whoAmI('Jill', 27);
+whoAmI('Jill', '27');

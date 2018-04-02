00b40b6bcbdeddfb3119b03699ae400c9e221416
Craig Pounds <nweaver@windstream.net>
2018-04-02 13:45:48 -0500

    yearOfBirth function

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function whoAmI(name, age) {
-  const yearOfBirth = 2018 - age;
   console.log('Hi my chris is Doug and I\'m 29 years ol to the console.');
-  console.log('I was born in ' + yearOfBirth);
+  console.log('I was born in ' + yearOfBirth(age));
+}
+function yearOfBirth(age) {
+  return yearOfBirth = 2018 - age;
 }
 whoAmI('chris', 25);

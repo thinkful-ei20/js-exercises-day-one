3435b1757adef7a389c367706baf32789cb2f960
Craig Pounds <nweaver@windstream.net>
2018-04-02 13:35:57 -0500

    yet another commit

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function whoAmI() {
-  console.log('Hi my name is Doug and I\'m 29 years ol to the console.');
+function whoAmI(name, age) {
+  console.log('Hi my ${name} is Doug and I\'m ${age} years ol to the console.');
 }
 whoAmI();
